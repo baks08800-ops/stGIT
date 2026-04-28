ddd1869e3edacfba8031ec5cd5ed9daaacd37547
Alex <alex74111@mail.ru>
2026-04-28 10:54:22 +0500

    Pr2

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::group(['prefix'=>'admin','namespace'=>'Admin'], function(){
     Route::get('/',[MainController::class,'index'])->name('admin.index');
 });
